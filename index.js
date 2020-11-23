cd53eb7eb818247a28195176616acd2c7c3c498e
Nataliya Rezvushkina <rezvushkina@gmail.com>
2020-11-23 16:06:55 +0300

    fix: справление ошибок для popup

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ let popup = document.querySelector('.popup');
 let popupCloseButton = document.querySelector('.popup__close-button');
 let formButton = document.querySelector('.form__button');
 
-
+let nameElement = document.querySelector('.profile__title');
+let jobElement = document.querySelector('.profile__subtitle');
 profileeditbutton.addEventListener('click', togglePopupVisibility);
+profileeditbutton.addEventListener('click', assignValueInput);
 popupCloseButton.addEventListener('click', togglePopupVisibility);
+
+function assignValueInput() {
+    let nameInput = document.getElementById('name');
+    let jobInput = document.getElementById('job');
+    nameInput.value = nameElement.textContent;
+    jobInput.value = jobElement.textContent;
+
+}
 
 function togglePopupVisibility() {
     popup.classList.toggle('popup_visible');
@@ -13,19 +23,13 @@ function togglePopupVisibility() {
 
 let formElement = document.querySelector('.form');
 
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
     evt.preventDefault();
     let nameInput = document.getElementById('name');
     let jobInput = document.getElementById('job');
-
-    let nameElement = document.querySelector('.profile__title');
-    let jobElement = document.querySelector('.profile__subtitle');
-    nameInput.value.textContent=nameElement;
-    console.log(nameElement);
-    //nameElement.textContent=nameInput.value;
-    console.log(nameInput.value);
-    //jobElement.textContent=jobInput.value;
+    nameElement.textContent = nameInput.value;
+    jobElement.textContent = jobInput.value;
     togglePopupVisibility();
-
 }
+
 formElement.addEventListener('submit', formSubmitHandler);
