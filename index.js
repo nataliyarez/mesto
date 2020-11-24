5c6e8ef2c6fabcd39feac4656ae047bfd1c1e994
Nataliya Rezvushkina <rezvushkina@gmail.com>
2020-11-24 16:41:02 +0300

    fix: справление ошибок для popup

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 let profileeditbutton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let popupCloseButton = document.querySelector('.popup__close-button');
-let formButton = document.querySelector('.form__button');
+
+
+let nameInput = document.getElementById('name');
+let jobInput = document.getElementById('job');
 
 let nameElement = document.querySelector('.profile__title');
 let jobElement = document.querySelector('.profile__subtitle');
-profileeditbutton.addEventListener('click', togglePopupVisibility);
-profileeditbutton.addEventListener('click', assignValueInput);
-popupCloseButton.addEventListener('click', togglePopupVisibility);
+
 
 function assignValueInput() {
-    let nameInput = document.getElementById('name');
-    let jobInput = document.getElementById('job');
     nameInput.value = nameElement.textContent;
     jobInput.value = jobElement.textContent;
+    popup.classList.add('popup_visible');
 
 }
 
@@ -25,11 +25,11 @@ let formElement = document.querySelector('.form');
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    let nameInput = document.getElementById('name');
-    let jobInput = document.getElementById('job');
     nameElement.textContent = nameInput.value;
     jobElement.textContent = jobInput.value;
     togglePopupVisibility();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+profileeditbutton.addEventListener('click', assignValueInput);
+popupCloseButton.addEventListener('click', togglePopupVisibility);
